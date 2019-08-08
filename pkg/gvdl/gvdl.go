7d8feaae96b524eb2cf2016391864fc84d3b0015
Diane Looney <diane@dianelooney.com>
2019-08-08 19:31:35 -0400

    Implement "add layer" syntax

--- pkg/gvdl/gvdl.go
+++ pkg/gvdl/gvdl.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/dianelooney/gvd/pkg/daemon"
 )
@@ -33,6 +34,8 @@ func execAdd(sc *bufio.Scanner, d *daemon.D) (err error) {
 	switch sc.Text() {
 	case "source":
 		return execAddSource(sc, d)
+	case "layer":
+		return execAddLayer(sc, d)
 	}
 	return fmt.Errorf("unrecognized add subcommand '%s'", sc.Text())
 }
@@ -42,9 +45,60 @@ func execAddSource(sc *bufio.Scanner, d *daemon.D) (err error) {
 		return io.ErrUnexpectedEOF
 	}
 	name := sc.Text()
+
 	if !sc.Scan() {
 		return io.ErrUnexpectedEOF
 	}
 	path := sc.Text()
+
 	return d.AddSource(name, path)
+}
+
+/*
+func execAddProgram(sc *bufio.Scanner, d *daemon.D) (err error) {
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	name := sc.Text()
+
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	pathV := sc.Text()
+
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	pathF := sc.Text()
+
+	return d.AddProgram(name, pathV, pathF)
+}
+*/
+
+func execAddLayer(sc *bufio.Scanner, d *daemon.D) (err error) {
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	name := sc.Text()
+
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	source := sc.Text()
+
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	program := sc.Text()
+
+	if !sc.Scan() {
+		return io.ErrUnexpectedEOF
+	}
+	depthA := sc.Text()
+	depthI, err := strconv.ParseFloat(depthA, 32)
+	if err != nil {
+		return err
+	}
+
+	return d.AddLayer(name, source, program, float32(depthI))
 }

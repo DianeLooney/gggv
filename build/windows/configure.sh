8708fb47c13eb7b11e3ac158cc310d4fe3c005d0
Diane Looney <diane@dianelooney.com>
2019-08-10 19:04:19 -0400

    More windows build fixes

--- build/windows/configure.sh
+++ build/windows/configure.sh
@@ -1,13 +1,13 @@
 mkdir ~/pkg-config
 
-( echo "name=libavcodec" ; cat base.pc ) > $PKG_CONFIG_PATH/libavcodec.pc;
-( echo "name=libavfilter" ; cat base.pc ) > $PKG_CONFIG_PATH/libavfilter.pc;
-( echo "name=libavutil" ; cat base.pc ) > $PKG_CONFIG_PATH/libavutil.pc;
-( echo "name=libswscale" ; cat base.pc ) > $PKG_CONFIG_PATH/libswscale.pc;
-( echo "name=libavdevice" ; cat base.pc ) > $PKG_CONFIG_PATH/libavdevice.pc;
-( echo "name=libavformat" ; cat base.pc ) > $PKG_CONFIG_PATH/libavformat.pc;
-( echo "name=libswresample" ; cat base.pc ) > $PKG_CONFIG_PATH/libswresample.pc;
-( echo "name=swresample" ; cat base.pc ) > $PKG_CONFIG_PATH/swresample.pc;
+( echo "name=libavcodec" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavcodec.pc;
+( echo "name=libavfilter" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavfilter.pc;
+( echo "name=libavutil" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavutil.pc;
+( echo "name=libswscale" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswscale.pc;
+( echo "name=libavdevice" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavdevice.pc;
+( echo "name=libavformat" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavformat.pc;
+( echo "name=libswresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswresample.pc;
+( echo "name=swresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/swresample.pc;
 
 curl https://ffmpeg.zeranoe.com/builds/win64/dev/ffmpeg-latest-win64-dev.zip > $HOME/ffmpeg-latest-win64-dev.zip
 unzip $HOME/ffmpeg-latest-win64-dev.zip -d $HOME/

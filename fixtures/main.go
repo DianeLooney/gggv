a427290cd52a604738fa5c972ba45c59bcaade41
Diane Looney <diane@dianelooney.com>
2019-08-11 03:19:34 -0400

    It kinda works for the leto-format

---
+++ fixtures/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/dianelooney/gvd/internal/ffmpeg"
+)
+
+var fixtures = []string{
+	"leto-1.mp4",
+}
+
+func main() {
+	d, err := ffmpeg.NewFileDecoder("leto-1.mp4")
+	if err != nil {
+		panic(err)
+	}
+	frame := d.NextFrame()
+
+}

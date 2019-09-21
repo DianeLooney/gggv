39e1a6ece4da5fa011350ae0ac8f48733338f891
John <themself@johnlp.xyz>
2019-09-21 16:20:45 -0400

    default frag shader is now a pass-thru with all the uniforms in it

--- shaders/frag/default.glsl
+++ shaders/frag/default.glsl
@@ -1,26 +1,34 @@
 #version 330
 
+//default uniforms from GGGV
+
+//futher sources are set as tex1, tex2, tex3
 uniform sampler2D tex0;
-uniform sampler2D tex1;
-uniform sampler2D tex2;
+
+//only set when tex* is a FFsource
+uniform float tex0Width;
+uniform float tex0Height;
+
+//seconds since application start. 
+//monotonically increasing, will never be 0 
 uniform float time;
-
+//time the last frame took to render
+uniform float renderTime;
+//number of frames in the last second
+uniform float fps;
+//output window size
+uniform float windowHeight;
 uniform float windowWidth;
+//cursor position
 uniform float cursorX;
+uniform float cursorY;
 
-uniform float ampl;
-
-uniform float randR;
-uniform float randG;
-uniform float randB;
-
+//texCoord
 in vec2 fragTexCoord;
-
+//output pixel color
 out vec4 outputColor;
 
-const float PI = 3.1415926535897932384626433832795;
 
 void main() {
-    //outputColor = texture(tex0, vec2(0,0));
     outputColor = texture(tex0, fragTexCoord);
 }

2545acfde083c26731545a886b42ed8418b90712
John Pasquarello <themself@johnlp.xyz>
2019-12-15 22:08:44 -0500

    rgb to hsv shaders (#34)
    * added a handful of nifty new shaders
    
    * rgb<->hsv conversion shaders

---
+++ shaders/frag/filt.hsv2rgb.glsl
@@ -0,0 +1,47 @@
+#version 330
+
+//default uniforms from GGGV
+
+//futher sources are set as tex1, tex2, tex3
+uniform sampler2D tex0;
+
+//result of the last render of this shader
+uniform sampler2D lastFrame;
+
+//only set when tex* is a FFsource
+uniform float tex0Width;
+uniform float tex0Height;
+
+//seconds since application start. 
+//monotonically increasing, will never be 0 
+uniform float time;
+//time the last frame took to render
+uniform float renderTime;
+//number of frames in the last second
+uniform float fps;
+//output window size
+uniform float windowHeight;
+uniform float windowWidth;
+//cursor position
+uniform float cursorX;
+uniform float cursorY;
+
+//texCoord
+in vec2 fragTexCoord;
+//output pixel color
+out vec4 outputColor;
+
+//neato code from the lolengine ppl
+//http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl#
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main() {
+  vec4 inp = texture(tex0, fragTexCoord);
+  vec3 rgb = hsv2rgb(inp.rgb);
+  outputColor = vec4(rgb.r,rgb.g,rgb.b,inp.a);
+}

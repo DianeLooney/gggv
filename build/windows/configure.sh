8faa73551c53903f6a24dfb3164f33fafdb1fdc8
Diane Looney <diane@dianelooney.com>
2019-12-26 13:25:34 -0500

    The things I do for love...

--- build/windows/configure.sh
+++ build/windows/configure.sh
@@ -1,10 +1,12 @@
 mkdir $PKG_CONFIG_PATH
 
-( echo "name=libavcodec" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavcodec.pc;
-( echo "name=libavfilter" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavfilter.pc;
-( echo "name=libavutil" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavutil.pc;
-( echo "name=libswscale" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswscale.pc;
-( echo "name=libavdevice" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavdevice.pc;
-( echo "name=libavformat" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavformat.pc;
-( echo "name=libswresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswresample.pc;
-( echo "name=swresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/swresample.pc;
+
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavcodec" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavcodec.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavfilter" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavfilter.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavutil" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavutil.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libswscale" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswscale.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavdevice" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavdevice.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavformat" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavformat.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libswresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswresample.pc;
+( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=swresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/swresample.pc;
+( echo "prefix=/tmp/portaudio"; echo "name=portaudio" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/swresample.pc;

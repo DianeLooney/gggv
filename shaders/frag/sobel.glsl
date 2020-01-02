a52bd59f54f5d854e6e5089b7f7c99fd37162342
Diane Looney <diane@dianelooney.com>
2020-01-01 22:39:18 -0500

    Refactor sobel shader

--- shaders/frag/sobel.glsl
+++ shaders/frag/sobel.glsl
@@ -35,51 +35,49 @@ out vec4 outputColor;
 uniform float strength;
 float internalStrength = strength == 0 ? 0.0001 : strength;
 
-mat3 x_kernel;
-mat3 y_kernel;
-
-
-void main() {
-  x_kernel[0] = vec3(-1, 0, 1);
-  x_kernel[1] = vec3(-2, 0, 2);
-  x_kernel[2] = vec3(-1, 0, 1);
-
-  y_kernel[0] = vec3( 1, 2, 1);
-  y_kernel[1] = vec3( 0, 0, 0);
-  y_kernel[2] = vec3(-1,-2,-1);
-
-  float x_step = 1/windowWidth;
-  float y_step = 1/windowHeight;
-
-  vec4 x_aggregator = vec4(0);
-  x_aggregator += x_kernel[0][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-  x_aggregator += x_kernel[0][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-  x_aggregator += x_kernel[0][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-
-  x_aggregator += x_kernel[1][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-  x_aggregator += x_kernel[1][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-  x_aggregator += x_kernel[1][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-
-  x_aggregator += x_kernel[2][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-  x_aggregator += x_kernel[2][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-  x_aggregator += x_kernel[2][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-
-  vec4 y_aggregator = vec4(0);
-  y_aggregator += y_kernel[0][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-  y_aggregator += y_kernel[0][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-  y_aggregator += y_kernel[0][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step*-1 + fragTexCoord.y)));
-
-  y_aggregator += y_kernel[1][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-  y_aggregator += y_kernel[1][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-  y_aggregator += y_kernel[1][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step* 0 + fragTexCoord.y)));
-
-  y_aggregator += y_kernel[2][0] * texture(tex0, vec2((x_step * -1 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-  y_aggregator += y_kernel[2][1] * texture(tex0, vec2((x_step *  0 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-  y_aggregator += y_kernel[2][2] * texture(tex0, vec2((x_step *  1 + fragTexCoord.x),(y_step* 1 + fragTexCoord.y)));
-
-
-  outputColor = sqrt(x_aggregator*x_aggregator+y_aggregator*y_aggregator);
-
-  outputColor.a = 1;
+vec4 kernel[3][3];
+vec4 dot(vec4 m1[3][3], mat3 m2) {
+  return (
+    m1[0][0] * m2[0][0] + 
+    m1[0][1] * m2[0][1] + 
+    m1[0][2] * m2[0][2] + 
+    m1[1][0] * m2[1][0] + 
+    m1[1][1] * m2[1][1] + 
+    m1[1][2] * m2[1][2] + 
+    m1[2][0] * m2[2][0] + 
+    m1[2][1] * m2[2][1] + 
+    m1[2][2] * m2[2][2]
+  );
 }
 
+void requireKernel() {
+  vec2 dx = vec2(1.0 / windowWidth, 0);
+  vec2 dy = vec2(0, 1.0 / windowHeight);
+  kernel[0][0] = texture(tex0, fragTexCoord + -1*dx + -1*dy);
+  kernel[0][1] = texture(tex0, fragTexCoord + -1*dx +  0*dy);
+  kernel[0][2] = texture(tex0, fragTexCoord + -1*dx +  1*dy);
+  kernel[1][0] = texture(tex0, fragTexCoord +  0*dx + -1*dy);
+  kernel[1][1] = texture(tex0, fragTexCoord +  0*dx +  0*dy);
+  kernel[1][2] = texture(tex0, fragTexCoord +  0*dx +  1*dy);
+  kernel[2][0] = texture(tex0, fragTexCoord +  1*dx + -1*dy);
+  kernel[2][1] = texture(tex0, fragTexCoord +  1*dx +  0*dy);
+  kernel[2][2] = texture(tex0, fragTexCoord +  1*dx +  1*dy);
+}
+
+void main() {
+  requireKernel();
+  mat3 xWeights = mat3(
+    -1, 0, 1,
+    -2, 0, 2,
+    -1, 0, 1
+  );
+  mat3 yWeights = mat3(
+     1,  2,  1,
+     0,  0,  0,
+    -1, -2, -1
+  );
+  vec4 xAgg = dot(kernel, xWeights);
+  vec4 yAgg = dot(kernel, yWeights);
+  outputColor = sqrt(xAgg * xAgg + yAgg * yAgg);
+  outputColor.a = 1;
+}

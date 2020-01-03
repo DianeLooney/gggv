7c4d69301f8ea70a52706d1dadf1b091e7b50c41
Fotios Lindiakos <fotioslindiakos@gmail.com>
2020-01-02 23:33:54 -0500

    Made windows build script more dynamic

--- build/windows/configure.sh
+++ build/windows/configure.sh
@@ -1,12 +1,30 @@
-mkdir $PKG_CONFIG_PATH
+set -euo pipefail
 
+mkdir -p $PKG_CONFIG_PATH
 
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavcodec" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavcodec.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavfilter" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavfilter.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavutil" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavutil.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libswscale" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswscale.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavdevice" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavdevice.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libavformat" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libavformat.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=libswresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/libswresample.pc;
-( echo "prefix=/tmp/ffmpeg-latest-win64-dev"; echo "name=swresample" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/swresample.pc;
+declare -A libs
+
+# github.com/giorgisio/goav
+libs["ffmpeg-latest-win64-dev"]="
+	libavcodec
+	libavfilter
+	libavutil
+	libavdevice
+	libavformat
+	libswscale
+	libswresample
+	swresample
+"
+
+for prefix in "${!libs[@]}"; do
+	for lib in ${libs[$prefix]}; do
+		cat <<-EOM >${PKG_CONFIG_PATH}/${lib}.pc
+			prefix=/tmp/${prefix}
+			name=${lib}
+
+			$(<build/windows/base.pc)
+		EOM
+	done
+done
+
 # ( echo "prefix=/tmp/portaudio"; echo "name=portaudio-2.0" ; cat build/windows/base.pc ) > $PKG_CONFIG_PATH/portaudio-2.0.pc;

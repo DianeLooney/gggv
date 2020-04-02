d84a251702e3af45d12640ccfc4f89ce14ac3928
Diane Looney <diane@dianelooney.com>
2020-04-01 23:19:21 -0400

    Fix lint

--- shaders/vert/default.glsl
+++ shaders/vert/default.glsl
@@ -2,5 +2,5 @@ void main() {
   geomTexCoord = vertTexCoord;
   geomScreenCoord = geomTexCoord * vec2(windowWidth + 1, windowHeight + 1);
   geomParticleN = vertParticleN;
-  gl_Position = vec4(vert*2, 1);
+  gl_Position = vec4(vert * 2, 1);
 }
